5d2125ee26a9f2333968b5cd544720e2d4434a43
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-08-26 19:30:08 +0530

    basic expressjs setup

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("App is running"));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
