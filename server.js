9abd35617608e41a1b49528e0a6392f2ada203c5
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-08-26 19:43:29 +0530

    dotenv configured

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => res.send("App is running"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
