35de014d5ee112489ce0c74b9f18339d865733f5
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-08-26 19:48:08 +0530

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => res.send("App is running"));
