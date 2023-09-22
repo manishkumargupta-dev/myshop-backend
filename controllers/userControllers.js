6c5018e551d7e8f4450f7e2a66a6bb4f576045c3
Manish Kumar Gupta <manishkumargupta875@gmail.com>
2023-09-22 18:00:57 +0530

    login endpoint

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,31 @@
 import asyncHandler from "./../middlewares/asyncHandler.js";
+import User from "./../models/userModel.js";
+import bcrypt from "bcryptjs";
+import AppError from "../helpers/AppError.js";
+import jwt from "jsonwebtoken";
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("loginUser");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 1000,
+    });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    throw new AppError(401, "Invalid email or password");
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("App is running"));
